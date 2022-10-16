3329a5b5cbafcad633a78a83a90d8b31c10de680
SanjayKumarUppala <sanjaypro8b@gmail.com>
2022-10-16 11:35:40 +0530

    Change Name

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,7 +20,7 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'Forms'`, () => {
+  it(`should have as title 'Vaccine Management'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
     expect(app.title).toEqual('VaccineManagemnt');
@@ -30,6 +30,6 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('Forms app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('Vaccine Management app is running!');
   });
 });
